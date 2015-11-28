6683c7c66e93609751beb49b4c2585b6bd946a26
gbarton <garrett.barton@gmail.com>
2015-11-27 23:21:45 -0500

    about to make custom network manager

---
+++ Assets/Scripts/NetworkManagerCustom.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class NetworkManagerCustom :NetworkManager {
+
+	public void StartupHost() {
+		SetPort ();
+		NetworkManager.singleton.StartHost ();
+	}
+
+	public void JoinGame() {
+		SetIPAddress ();
+		SetPort ();
+		NetworkManager.singleton.StartClient ();
+	}
+
+	void SetupIPAddress() {
+		string ipAddress = GameObject.Find ("InputFieldIPAddress").transform.FindChild ("Text").GetComponent<Text> ().text;
+		NetworkManager.singleton.networkAddress = ipAddress;
+	}
+
+	void SetPort() {
+		NetworkManager.singleton.networkPort = 7777;
+	}
+
+
+}

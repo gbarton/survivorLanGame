ef69cd0805138605b995f3abf7782ca7f67e7bb5
gbarton <garrett.barton@gmail.com>
2015-11-28 11:17:36 -0500

    moved starting positions into temp building

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// master manager of the entire game state
+public class GameManager : MonoBehaviour {
+
+	public float startDelay = 5f;
+	public float endDelay = 5f;
+
+
+
+	// Use this for initialization
+	void OnStartServer () {
+		GameObject[] players = GameObject.FindGameObjectsWithTag ("Player");
+		//for(GameObject p  players) {
+		//	p
+		//}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
